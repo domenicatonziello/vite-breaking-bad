b6640bcaaa8e4b06b3251324085f8b90b7703216
domenicatonziello <domenica.tonziello10@gmail.com>
2023-01-24 15:24:52 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+// importo font
+import "@fontsource/abeezee";
+// importo bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App.vue'
+createApp(App).mount('#app')
